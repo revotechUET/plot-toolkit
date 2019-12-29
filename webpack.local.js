c570b4081fed77ca9379b3d6d0cd7087a890f930
phucnv282 <phucnv282@gmail.com>
2019-12-29 18:56:06 +0700

    update

---
+++ webpack.local.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const webpack = require('webpack');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    context: __dirname + '/src',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, '../wi-angular/watch/bower_components/plot-toolkit/dist'),
+        filename: 'plot-toolkit.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.html$/,
+            use: ['html-loader']
+        }, {
+            test: /\.less$/,
+            use: ['style-loader', 'css-loader', 'less-loader']
+
+        }]
+    },
+    plugins: [
+        new webpack.DllReferencePlugin({
+            context: __dirname,
+            manifest: require('./dist/library/library.json')
+        }),
+        new HardSourceWebpackPlugin()
+    ]
+}
