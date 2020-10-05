f8c2a61debe88c271a75e32b34d43486be918e57
Quang LN <quangln2810@gmail.com>
2020-10-05 11:41:34 +0700

    update

--- src/line-point-layer/index.js
+++ src/line-point-layer/index.js
@@ -11,6 +11,7 @@ angular.module(moduleName).component(name, component({
     controller: LPLayerController,
     bindings: {
         curveFunctionName: '<',
+        plotType: "<",
     }
 }));
 LPLayerController.$inject = ['$scope', '$timeout', '$element'];
@@ -19,26 +20,30 @@ function LPLayerController($scope, $timeout, $element) {
     AbstractLayerController.call(this, $timeout, $element, $scope);
     this.watchProperties = this.watchProperties.concat([
         "lineData",
+        "plotType"
     ]);
     this.defaultBindings = function() {
         this.lineStyleDefault();
     }
-    this.doAutofit = function() {
-        this.updateMaxY(d3.max(this.getData(), function(point,i) {return self.getY(point,i);}));
+    this.doAutofit = function () {
+        const getY = self.plotType === 'percentage' ? (d, i) => self.getY(d, i) / binSum * 100 : (d, i) => self.getY(d, i);
+        let maxY = d3.max(this.getData(), function (point, i) { return getY(point, i); });
+        maxY > 1 ? _.ceil(maxY, -1 * maxY.toString().length + 1) : maxY;
+        this.updateMaxY(maxY);
     }
     this.getLine = function() {
         let transform = this.getTransform();
         let orthoTransform = this.getOrthoTransform();
+        const binSum = self.getData().flat().length;
+        const getY = self.plotType === 'percentage' ? (d, i) => self.getY(d, i) / binSum * 100 : (d, i) => self.getY(d, i);
         let line = d3.line().curve(d3[this.curveFunctionName || 'curveNatural'])
             .x((d, i) => {
                 return transform(self.getX(d, i));
             })
-            .y((d, i) => {
-                return orthoTransform(self.getY(d, i));
-            })
+            .y((d, i) => orthoTransform(getY(d, i)))
             .defined(function (d, i) {
                 let x = self.getX(d, i);
-                let y = self.getY(d, i);
+                let y = getY(d, i);
                 return !isNaN(x) && !isNaN(y)
                     && y != Infinity && y != -Infinity
                     && !isNaN(transform(x)) && !isNaN(orthoTransform(y))
